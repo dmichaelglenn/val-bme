db408bacc7d03ea2d7310d711b8736a447399e10
Michael Glenn <michael@valentinecreative.co>
2018-07-17 17:47:03 -0700

    initial setup with new boilerplate

--- elementor-starter.php
+++ elementor-starter.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * Plugin Name: Elementor Starter
- * Description: A starter Elementor boilerplate for rapid addon development
- * Plugin URI: https://designsbynore.com/
- * Author: Zulfikar Nore
+ * Plugin Name: Bodymovin Elementor
+ * Description: Extends Elementor with the ability to incorporate Bodymovin renders within pages
+ * Plugin URI: https://valentinecreative.co
+ * Author: Valentine Creative
  * Version: 1.0.0
- * Author URI: https://designsbynore.com/
+ * Author URI: https://valentinecreative.co
  *
- * Text Domain: elementor-show
+ * Text Domain: val-bodymovin
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-define( 'ELEMENTOR_STARTER_VERSION', '1.0.0' );
+define( 'BODYMOVIN_ELEMENTOR_VERSION', '1.0.0' );
 
-define( 'ELEMENTOR_STARTER__FILE__', __FILE__ );
-define( 'ELEMENTOR_STARTER_PLUGIN_BASE', plugin_basename( ELEMENTOR_STARTER__FILE__ ) );
-define( 'ELEMENTOR_STARTER_PATH', plugin_dir_path( ELEMENTOR_STARTER__FILE__ ) );
-define( 'ELEMENTOR_STARTER_MODULES_PATH', ELEMENTOR_STARTER_PATH . 'modules/' );
-define( 'ELEMENTOR_STARTER_URL', plugins_url( '/', ELEMENTOR_STARTER__FILE__ ) );
-define( 'ELEMENTOR_STARTER_ASSETS_URL', ELEMENTOR_STARTER_URL . 'assets/' );
-define( 'ELEMENTOR_STARTER_MODULES_URL', ELEMENTOR_STARTER_URL . 'modules/' );
+define( 'BODYMOVIN_ELEMENTOR__FILE__', __FILE__ );
+define( 'BODYMOVIN_ELEMENTOR_PLUGIN_BASE', plugin_basename( BODYMOVIN_ELEMENTOR__FILE__ ) );
+define( 'BODYMOVIN_ELEMENTOR_PATH', plugin_dir_path( BODYMOVIN_ELEMENTOR__FILE__ ) );
+define( 'BODYMOVIN_ELEMENTOR_MODULES_PATH', BODYMOVIN_ELEMENTOR_PATH . 'modules/' );
+define( 'BODYMOVIN_ELEMENTOR_URL', plugins_url( '/', BODYMOVIN_ELEMENTOR__FILE__ ) );
+define( 'BODYMOVIN_ELEMENTOR_ASSETS_URL', BODYMOVIN_ELEMENTOR_URL . 'assets/' );
+define( 'BODYMOVIN_ELEMENTOR_MODULES_URL', BODYMOVIN_ELEMENTOR_URL . 'modules/' );
 
 /**
  * Load gettext translate for our text domain.
@@ -29,23 +29,23 @@ define( 'ELEMENTOR_STARTER_MODULES_URL', ELEMENTOR_STARTER_URL . 'modules/' );
  *
  * @return void
  */
-function elementor_starter_load_plugin() {
+function bodymovin_elementor_load_plugin() {
 	load_plugin_textdomain( 'elementor-starter' );
 
 	if ( ! did_action( 'elementor/loaded' ) ) {
-		add_action( 'admin_notices', 'elementor_starter_fail_load' );
+		add_action( 'admin_notices', 'bodymovin_elementor_fail_load' );
 		return;
 	}
 
 	$elementor_version_required = '1.0.6';
 	if ( ! version_compare( ELEMENTOR_VERSION, $elementor_version_required, '>=' ) ) {
-		add_action( 'admin_notices', 'elementor_starter_fail_load_out_of_date' );
+		add_action( 'admin_notices', 'bodymovin_elementor_fail_load_out_of_date' );
 		return;
 	}
 
-	require( ELEMENTOR_STARTER_PATH . 'plugin.php' );
+	require( BODYMOVIN_ELEMENTOR_PATH . 'plugin.php' );
 }
-add_action( 'plugins_loaded', 'elementor_starter_load_plugin' );
+add_action( 'plugins_loaded', 'bodymovin_elementor_load_plugin' );
 
 /**
  * Show in WP Dashboard notice about the plugin is not activated.
@@ -54,7 +54,7 @@ add_action( 'plugins_loaded', 'elementor_starter_load_plugin' );
  *
  * @return void
  */
-function elementor_starter_fail_load() {
+function bodymovin_elementor_fail_load() {
 	$screen = get_current_screen();
 	if ( isset( $screen->parent_file ) && 'plugins.php' === $screen->parent_file && 'update' === $screen->id ) {
 		return;
@@ -69,7 +69,7 @@ function elementor_starter_fail_load() {
 
 		$activation_url = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin );
 
-		$message = '<p>' . __( 'Elementor Starter is not working because you need to activate the Elementor plugin.', 'elementor-starter' ) . '</p>';
+		$message = '<p>' . __( 'Bodymovin Elementor is not working because you need to activate the Elementor plugin.', 'elementor-starter' ) . '</p>';
 		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $activation_url, __( 'Activate Elementor Now', 'elementor-starter' ) ) . '</p>';
 	} else {
 		if ( ! current_user_can( 'install_plugins' ) ) {
@@ -78,14 +78,14 @@ function elementor_starter_fail_load() {
 
 		$install_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=elementor' ), 'install-plugin_elementor' );
 
-		$message = '<p>' . __( 'Elementor Starter is not working because you need to install the Elemenor plugin', 'elementor-starter' ) . '</p>';
+		$message = '<p>' . __( 'Bodymovin Elementor is not working because you need to install the Elementor plugin', 'elementor-starter' ) . '</p>';
 		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $install_url, __( 'Install Elementor Now', 'elementor-starter' ) ) . '</p>';
 	}
 
 	echo '<div class="error"><p>' . $message . '</p></div>';
 }
 
-function elementor_starter_fail_load_out_of_date() {
+function bodymovin_elementor_fail_load_out_of_date() {
 	if ( ! current_user_can( 'update_plugins' ) ) {
 		return;
 	}
@@ -93,7 +93,7 @@ function elementor_starter_fail_load_out_of_date() {
 	$file_path = 'elementor/elementor.php';
 
 	$upgrade_link = wp_nonce_url( self_admin_url( 'update.php?action=upgrade-plugin&plugin=' ) . $file_path, 'upgrade-plugin_' . $file_path );
-	$message = '<p>' . __( 'Elementor Starter is not working because you are using an old version of Elementor.', 'elementor-starter' ) . '</p>';
+	$message = '<p>' . __( 'Bodymovin Elementor is not working because you are using an old version of Elementor.', 'elementor-starter' ) . '</p>';
 	$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $upgrade_link, __( 'Update Elementor Now', 'elementor-starter' ) ) . '</p>';
 
 	echo '<div class="error">' . $message . '</div>';
